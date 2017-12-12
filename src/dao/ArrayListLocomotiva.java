31759ad7dc71bf1d68eb721010e47dad24c5d60a
gihcapel <gihccapel@gmail.com>
2017-12-12 13:32:03 -0200

    Update ArrayListLocomotiva.java

--- src/dao/ArrayListLocomotiva.java
+++ src/dao/ArrayListLocomotiva.java
@@ -4,29 +4,51 @@ import java.util.ArrayList;
 
 import entity.Locomotiva;
 
+/**
+ *
+ * @author Agnes Travalon, Ana Carolina Lopes, Giovanna Capel e Pedro de Souza
+ * Moraes
+ *
+ * Classe para trabalhar a lista de arrays de locomotivas
+ */
 public final class ArrayListLocomotiva {
-		static ArrayList <Locomotiva> locomotivasdisponiveis;
-		private static ArrayListLocomotiva instance = null;
-		private ArrayListLocomotiva(ArrayList<Locomotiva> locomotivas) {}
-		public static ArrayListLocomotiva getInstance() {
-		if (instance==null) {
-		
-		instance = new ArrayListLocomotiva(locomotivasdisponiveis);
-		}
-		return instance;
-		}
-		public static ArrayList<Locomotiva> getLocomotivasdisponiveis() {
-			return locomotivasdisponiveis;
-		}
-		public static void setLocomotivasdisponiveis(ArrayList<Locomotiva> locomotivasdisponiveis) {
-			ArrayListLocomotiva.locomotivasdisponiveis = locomotivasdisponiveis;
-		}
-		public static void setInstance(ArrayListLocomotiva instance) {
-			ArrayListLocomotiva.instance = instance;
-		}
-		public void add(Locomotiva locomotiva) {
-			locomotivasdisponiveis.add(locomotiva);
-			
-		}
-		
-		}
+    //Componentes da classe
+
+    static ArrayList<Locomotiva> locomotivasdisponiveis;
+    private static ArrayListLocomotiva instance = null;
+
+    private ArrayListLocomotiva(ArrayList<Locomotiva> locomotivas) {
+    }
+
+    //Método que obtém a instância e verifica se é nula
+    //Se for nula, instancia um ArrayListLocomotiva
+    public static ArrayListLocomotiva getInstance() {
+        if (instance == null) {
+
+            instance = new ArrayListLocomotiva(locomotivasdisponiveis);
+        }
+        return instance;
+    }
+
+    //Obtém as locomotivas disponíveis
+    public static ArrayList<Locomotiva> getLocomotivasdisponiveis() {
+        return locomotivasdisponiveis;
+    }
+
+    //Insere as locomotivas disponíveis
+    public static void setLocomotivasdisponiveis(ArrayList<Locomotiva> locomotivasdisponiveis) {
+        ArrayListLocomotiva.locomotivasdisponiveis = locomotivasdisponiveis;
+    }
+
+    //Insere a instância
+    public static void setInstance(ArrayListLocomotiva instance) {
+        ArrayListLocomotiva.instance = instance;
+    }
+
+    //Método que adiciona uma nova locomotiva nas disponíveis
+    public void add(Locomotiva locomotiva) {
+        locomotivasdisponiveis.add(locomotiva);
+
+    }
+
+}

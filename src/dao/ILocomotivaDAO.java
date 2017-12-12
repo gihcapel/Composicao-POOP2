3d328fc999a541666cf8edd02553f66a0a4a23c7
gihcapel <gihccapel@gmail.com>
2017-12-12 11:40:16 -0200

    Update ILocomotivaDAO.java

--- src/dao/ILocomotivaDAO.java
+++ src/dao/ILocomotivaDAO.java
@@ -4,14 +4,24 @@ import java.util.List;
 
 import entity.Locomotiva;
 
-
+/**
+ * 
+ * @author Agnes Travalon, Ana Carolina Lopes, Giovanna Capel e Pedro de Souza Moraes
+ *
+ * Interface para abstrair o acesso aos dados das locomotivas, encapsulando os
+ * mesmos
+ */
 public interface ILocomotivaDAO {
 
+    //Salva a locomotiva instanciada
     int save(Locomotiva locomotiva);
 
+    //Atualiza a locomotiva instanciada
     int update(Locomotiva locomotiva);
 
+    //Remove a locomotiva
     int remove(Long id);
 
+    //Acha todas as locomotivas inseridas na lista
     List<Locomotiva> findAll();
 }

6198a046363c900ba74184f343e775ce6239a78c
gihcapel <gihccapel@gmail.com>
2017-12-12 11:37:04 -0200

    Update VagaoFacade.java

--- src/facade/VagaoFacade.java
+++ src/facade/VagaoFacade.java
@@ -1,33 +1,42 @@
 package facade;
 
-
-
 import java.util.List;
 
 import dao.IVagaoDAO;
 import dao.VagaoDAO;
 import entity.Vagao;
 
+/**
+ * 
+ * @author Agnes Travalon, Ana Carolina Lopes, Giovanna Capel e Pedro de Souza Moraes
+ *
+ * Classe com o padrão de projeto Facade (fachada) implementado para vagões
+ */
 public class VagaoFacade {
 
     private IVagaoDAO dao;
 
+    //Construtor não parametrizado instancia VagaoDAO
     public VagaoFacade() {
         this.dao = new VagaoDAO();
     }
 
+    //Método para salvar o vagão instanciado
     public int save(Vagao vagao) {
         return dao.save(vagao);
     }
 
+    //Método para atualizar o vagão instanciado
     public int update(Vagao vagao) {
         return dao.update(vagao);
     }
 
+    //Método para remover o vagão 
     public int remove(Long id) {
         return dao.remove(id);
     }
 
+    //Método para achar todos os vagões dentro da lista
     public List<Vagao> findAll() {
         return dao.findAll();
     }

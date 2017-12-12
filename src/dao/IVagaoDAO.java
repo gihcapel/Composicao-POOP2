c70a3884414df2a0aa6c8b72fce6e81a03dd0db9
gihcapel <gihccapel@gmail.com>
2017-12-12 11:39:52 -0200

    Update IVagaoDAO.java

--- src/dao/IVagaoDAO.java
+++ src/dao/IVagaoDAO.java
@@ -4,14 +4,23 @@ import java.util.List;
 
 import entity.Vagao;
 
-
+/**
+ * 
+ * @author Agnes Travalon, Ana Carolina Lopes, Giovanna Capel e Pedro de Souza Moraes
+ *
+ * Interface para abstrair o acesso aos dados dos vagões, encapsulando os mesmos
+ */
 public interface IVagaoDAO {
 
+    //Salva o vagão instanciado
     int save(Vagao vagao);
 
+    //Atualiza o vagão instanciado
     int update(Vagao vagao);
 
+    //Remove o vagão
     int remove(Long id);
 
+    //Acha todos os vagões inseridos na lista
     List<Vagao> findAll();
 }

6cfa24eab0f1a17d482452f423e930fe959830bd
gihcapel <gihccapel@gmail.com>
2017-12-12 13:30:52 -0200

    Update Composicao.java

--- src/entity/Composicao.java
+++ src/entity/Composicao.java
@@ -2,82 +2,100 @@ package entity;
 
 import java.util.ArrayList;
 
+/**
+ *
+ * @author Agnes Travalon, Ana Carolina Lopes, Giovanna Capel e Pedro de Souza
+ * Moraes
+ *
+ * Classe para trabalhar com as composições
+ */
 public class Composicao {
-	private String codigo, descricao, comprimento, bitola, pesoMax;
-	protected ArrayList<Locomotiva> locomotivasselecionadas = new ArrayList<Locomotiva>();
-	protected ArrayList<Vagao> vagoesselecionados = new ArrayList<Vagao>();
-	private static final int MAXLOCOMOTIVA  = 3;
-	public Composicao(ArrayList locomotivasselecionadas, ArrayList vagoesselecionados) {
-		this.locomotivasselecionadas = locomotivasselecionadas;
-		this.vagoesselecionados = vagoesselecionados;
-	}
-	
-	public String getCodigo() {
-		return codigo;
-	}
 
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
+    //Componentes da classe
+    private String codigo, descricao, comprimento, bitola, pesoMax;
+    protected ArrayList<Locomotiva> locomotivasselecionadas = new ArrayList<Locomotiva>();
+    protected ArrayList<Vagao> vagoesselecionados = new ArrayList<Vagao>();
+    private static final int MAXLOCOMOTIVA = 3;
 
-	public String getDescricao() {
-		return descricao;
-	}
+    //Construtor parametrizado
+    public Composicao(ArrayList locomotivasselecionadas, ArrayList vagoesselecionados) {
+        this.locomotivasselecionadas = locomotivasselecionadas;
+        this.vagoesselecionados = vagoesselecionados;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    //Obtém código
+    public String getCodigo() {
+        return codigo;
+    }
+    
+    //Insere código
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    //Obtém descrição
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    //Insere descrição
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    //Obtém comprimento
+    public String getComprimento() {
+        return comprimento;
+    }
+    
+    //Insere comprimento
+    public void setComprimento(String comprimento) {
+        this.comprimento = comprimento;
+    }
+    
+    //Obtém bitola
+    public String getBitola() {
+        return bitola;
+    }
+    
+    //Insere bitola
+    public void setBitola(String bitola) {
+        this.bitola = bitola;
+    }
+    
+    //Obtém peso maximo
+    public String getPesoMax() {
+        return pesoMax;
+    }
+    
+    //Insere peso maximo
+    public void setPesoMax(String pesoMax) {
+        this.pesoMax = pesoMax;
+    }
+    
+    //Obtém as locomotivas selecionadas de dentro do array
+    public ArrayList<Locomotiva> getLocomotivasselecionadas() {
+        return locomotivasselecionadas;
+    }
+    
+    //Insere as locomotivas selecionadas de dentro do array
+    public void setLocomotivasselecionadas(ArrayList<Locomotiva> locomotivasselecionadas) {
+        this.locomotivasselecionadas = locomotivasselecionadas;
+    }
+    
+    //Obtém os vagões selecionados de dentro do array
+    public ArrayList<Vagao> getVagoesselecionados() {
+        return vagoesselecionados;
+    }
+    
+    //Insere os vagões selecionados de dentro do array
+    public void setVagoesselecionados(ArrayList<Vagao> vagoesselecionados) {
+        this.vagoesselecionados = vagoesselecionados;
+    }
+    
+    //Obtém o peso maximo permitido na locomotiva
+    public static int getMaxlocomotiva() {
+        return MAXLOCOMOTIVA;
+    }
 
-	public String getComprimento() {
-		return comprimento;
-	}
-
-	public void setComprimento(String comprimento) {
-		this.comprimento = comprimento;
-	}
-
-	public String getBitola() {
-		return bitola;
-	}
-
-	public void setBitola(String bitola) {
-		this.bitola = bitola;
-	}
-
-	public String getPesoMax() {
-		return pesoMax;
-	}
-
-	public void setPesoMax(String pesoMax) {
-		this.pesoMax = pesoMax;
-	}
-
-	public ArrayList<Locomotiva> getLocomotivasselecionadas() {
-		return locomotivasselecionadas;
-	}
-
-	public void setLocomotivasselecionadas(ArrayList<Locomotiva> locomotivasselecionadas) {
-		this.locomotivasselecionadas = locomotivasselecionadas;
-	}
-
-	public ArrayList<Vagao> getVagoesselecionados() {
-		return vagoesselecionados;
-	}
-
-	public void setVagoesselecionados(ArrayList<Vagao> vagoesselecionados) {
-		this.vagoesselecionados = vagoesselecionados;
-	}
-
-	public static int getMaxlocomotiva() {
-		return MAXLOCOMOTIVA;
-	}
-
-	@Override
-	public String toString() {
-		return "Composicao [codigo=" + codigo + ", descricao=" + descricao + ", comprimento=" + comprimento
-				+ ", bitola=" + bitola + ", pesoMax=" + pesoMax + ", locomotivascomposicao=" + locomotivasselecionadas
-				+ ", vagoescomposicao=" + vagoesselecionados + "]";
-	}
-	
-	
 }
